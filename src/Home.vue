1b7648f11883e9e2cc803e5cec8db64570b2a6d1
yuma533 <o.g.w.y.u.m2262@gmail.com>
2023-04-06 18:36:35 +0900

    改善する必要あり。

---
+++ src/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  data: {
+    name: 'yuma'
+  }
+}
+</script>
--- src/TodoApp.vue
+++ src/TodoApp.vue
@@ -1,14 +1,39 @@
 <script>
 import todoView from './View/todoView.vue';
+import Home from './Home.vue'
+
+const routes = {
+  '/home': Home,
+  '/todoView': todoView
+}
 
 export default {
 	  components: {
+      Home,
 	    todoView
-	  }
+	  },
+    //意味を理解する
+    data() {
+    return {
+      currentPath: window.location.hash
+    }
+    },
+    computed: {
+      currentView() {
+        return routes[this.currentPath.slice(1) || '/'] || NotFound
+      }
+    },
+    mounted() {
+      window.addEventListener('hashchange', () => {
+		    this.currentPath = window.location.hash
+		  })
+    }
 	}
 </script>
 
 
 <template>
-  <todoView />
+  <a href="#/home">Home</a>
+  <a href="#/todoView">TodoView</a>
+  <component :is="currentView" />
 </template>
