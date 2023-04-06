853a5947ab836ee8fa799377e0317ebe086fce15
yuma533 <o.g.w.y.u.m2262@gmail.com>
2023-04-06 23:10:01 +0900

    nanikore

--- src/TodoApp.vue
+++ src/TodoApp.vue
@@ -1,36 +1,12 @@
 <script>
 import todoView from './View/todoView.vue';
-import Home from './Home.vue'
-
-const routes = {
-  '/home': Home,
-  '/todoView': todoView
-}
 
 export default {
 	  components: {
-      Home,
 	    todoView
-	  },
-    //意味を理解する
-    data() {
-    return {
-      currentPath: window.location.hash
-    }
-    },
-    computed: {
-      currentView() {
-        return routes[this.currentPath.slice(1) || '/'] || NotFound
-      }
-    },
-    mounted() {
-      window.addEventListener('hashchange', () => {
-		    this.currentPath = window.location.hash
-		  })
     }
 	}
 </script>
-
 
 <template>
   <a href="#/home">Home</a>
